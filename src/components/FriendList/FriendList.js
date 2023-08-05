40129a665e20cfb351a58d2f3c7d5b0f5c0b3595
Artur <110535829+Primus1C@users.noreply.github.com>
2023-08-05 21:29:22 +0200

    develop

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,17 +1,17 @@
-import PropTypes from "prop-types";
-import FriendListItem from "../FriendListItem/FriendListItem";
-import styles from "./FriendList.css"
+import PropTypes from 'prop-types';
+import FriendListItem from '../FriendListItem/FriendListItem';
+import './FriendList.css';
 
 const FriendList = ({ friends }) => {
-    return (
-        <ul className="friendlist__friend-list">
-            {friends.map((element) => FriendListItem(element))}
-        </ul>
-    );
+  return (
+    <ul className="friendlist__friend-list">
+      {friends.map(element => FriendListItem(element))}
+    </ul>
+  );
 };
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(PropTypes.shape),
+  friends: PropTypes.arrayOf(PropTypes.shape),
 };
 
 export default FriendList;
--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,39 +1,39 @@
-import PropTypes from "prop-types";
-import styles from "./TransactionHistory.css";
+import PropTypes from 'prop-types';
+import './TransactionHistory.css';
 
 const TransactionHistory = ({ items }) => {
-    return (
-        <table className="transaction__history">
-            <thead>
-                <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
-                </tr>
-            </thead>
+  return (
+    <table className="transaction__history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
 
-            <tbody>
-                {items.map((element) => (
-                    <tr key={element.id}>
-                        <td>{element.type}</td>
-                        <td>{element.amount}</td>
-                        <td>{element.currency}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
+      <tbody>
+        {items.map(element => (
+          <tr key={element.id}>
+            <td>{element.type}</td>
+            <td>{element.amount}</td>
+            <td>{element.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 TransactionHistory.propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired,
-            amount: PropTypes.string.isRequired,
-            currency: PropTypes.string.isRequired,
-        })
-    ),
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 export default TransactionHistory;
